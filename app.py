5da25a950cadc9c393c5e6410a49cb9d6707a3a5
mitokande <benmithat18@gmail.com>
2022-10-24 15:53:54 +0300

    pandas and talib import

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+import pandas as pd
+import talib
+from binance import Client
 
 app = Flask(__name__)
 
@@ -10,4 +13,14 @@ def hello_world():
 
 @app.route("/b")
 def bye_world():
-    return ("BYE BYE World")
+    key = 'wrfybb7xo2Cvze0Ii0zOO8FNkWIX4UCIWtBdONPZH7PD5nmP10pWVGDig9zFuffF'
+    secret = 'oPEp31iGEumVcl9NLcDTkwq3Q8F3A653ua2QYy33N1puebUsTbNdQo5gc8kP4UOR'
+
+    client = Client(api_key = key, api_secret = secret)
+
+    data = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE,start_str = "15 minutes ago UTC")
+    #datah
+
+    df = pd.DataFrame(data, columns=['date','open', 'high', 'low', 'close', 'volume','close_time', 'qav', 'num_trades',
+                    'taker_base_vol', 'taker_quote_vol', 'ignore'])
+    return df
