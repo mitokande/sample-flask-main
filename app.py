3bf7c34be3202627efa4f7fb923df7b40312054c
mitokande <benmithat18@gmail.com>
2022-10-24 16:01:18 +0300

    comment talib

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template
 import pandas as pd
-import talib
+#import talib
 from binance import Client
 
 app = Flask(__name__)
@@ -13,14 +13,15 @@ def hello_world():
 
 @app.route("/b")
 def bye_world():
-    key = 'wrfybb7xo2Cvze0Ii0zOO8FNkWIX4UCIWtBdONPZH7PD5nmP10pWVGDig9zFuffF'
-    secret = 'oPEp31iGEumVcl9NLcDTkwq3Q8F3A653ua2QYy33N1puebUsTbNdQo5gc8kP4UOR'
+    # key = 'wrfybb7xo2Cvze0Ii0zOO8FNkWIX4UCIWtBdONPZH7PD5nmP10pWVGDig9zFuffF'
+    # secret = 'oPEp31iGEumVcl9NLcDTkwq3Q8F3A653ua2QYy33N1puebUsTbNdQo5gc8kP4UOR'
 
-    client = Client(api_key = key, api_secret = secret)
+    # client = Client(api_key = key, api_secret = secret)
 
-    data = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE,start_str = "15 minutes ago UTC")
-    #datah
+    # data = client.get_historical_klines('BTCUSDT', Client.KLINE_INTERVAL_1MINUTE,start_str = "15 minutes ago UTC")
+    # #datah
 
-    df = pd.DataFrame(data, columns=['date','open', 'high', 'low', 'close', 'volume','close_time', 'qav', 'num_trades',
-                    'taker_base_vol', 'taker_quote_vol', 'ignore'])
-    return df
+    # df = pd.DataFrame(data, columns=['date','open', 'high', 'low', 'close', 'volume','close_time', 'qav', 'num_trades',
+    #                 'taker_base_vol', 'taker_quote_vol', 'ignore'])
+    # return df
+    return ("talib")
