7b1258ab9db03a38b9e519caaa7de656c544868d
mitokande <benmithat18@gmail.com>
2022-10-24 18:48:10 +0300

    dynamic mfi indicator

--- app.py
+++ app.py
@@ -14,9 +14,29 @@ def hello_world():
 @app.route("/mfi")
 def mfi():
     args = request.args
-    no1 = args['interval']
-    no2 = args['coin']
-    return f'no {no1} no2 {no2}'
+    interval = args['interval']
+    coin = args['coin']
+    
+    client = Client(api_key = key, api_secret = secret)
+
+    match interval:
+        case "15m":
+            data = client.get_historical_klines(coin, Client.KLINE_INTERVAL_15MINUTE,start_str = "1 days ago UTC")
+        case "30m":
+            data = client.get_historical_klines(coin, Client.KLINE_INTERVAL_30MINUTE,start_str = "2 days ago UTC")
+        case "1h":
+            data = client.get_historical_klines(coin, Client.KLINE_INTERVAL_1HOUR,start_str = "4 days ago UTC")
+        case "4h":
+            data = client.get_historical_klines(coin, Client.KLINE_INTERVAL_4HOUR,start_str = "16 days ago UTC")
+        case "1d":
+            data = client.get_historical_klines(coin, Client.KLINE_INTERVAL_1DAY,start_str = "96 days ago UTC")
+    
+    df = pd.DataFrame(data, columns=['date','open', 'high', 'low', 'close', 'volume','close_time', 'qav', 'num_trades',
+                    'taker_base_vol', 'taker_quote_vol', 'ignore'])
+    result = talib.MFI(df["high"], df["low"], df["close"], df["volume"], timeperiod=14)
+
+    
+    return f'no {no1} no2 {no2} => {result[-1:]}'
 
 @app.route("/b")
 def b_world():
